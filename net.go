157c3a6b81fc208b4c013380aabc5b69afd4c840
Alejandro Mery <amery@jpi.io>
2023-01-29 17:20:14 +0000

    net: drop GetIPAddress() in favour of GetStringIPAddresses()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- net.go
+++ net.go
@@ -33,17 +33,6 @@ func GetInterfacesNames(except ...string) ([]string, error) {
 	return out, nil
 }
 
-// GetIPAddress tries to return the address we are most likely to use
-// to communicate with the network
-func GetIPAddress(ifaces ...string) (netip.Addr, error) {
-	// TODO: consider networks and return "best"
-	addrs, err := GetIPAddresses(ifaces...)
-	if len(addrs) > 0 {
-		return addrs[0], nil
-	}
-	return netip.Addr{}, err
-}
-
 // GetIPAddresses returns the list of IP Addresses,
 // optionally considering only the given interfaces
 func GetIPAddresses(ifaces ...string) ([]netip.Addr, error) {
