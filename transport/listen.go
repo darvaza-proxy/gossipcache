5583488b58be1cd32ea1e41c42bf4227d78b3243
Alejandro Mery <amery@jpi.io>
2023-03-10 16:56:41 +0000

    transport: introduce Listeners, a mechanism to provide pre-allocated listeners
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- transport/listen.go
+++ transport/listen.go
@@ -1,10 +1,88 @@
 package transport
 
-import "net"
+import (
+	"errors"
+	"fmt"
+	"net"
+
+	"github.com/darvaza-proxy/core"
+)
+
+var (
+	errBadSet = errors.New("invalid listener set")
+)
+
+// Listeners contains externally prepared listeners for our Transport
+type Listeners struct {
+	TCP []*net.TCPListener
+	UDP []*net.UDPConn
+}
+
+// Validate checks if the listeners are suitable, and returns
+// the addresses and port used
+func (lsn *Listeners) Validate() ([]string, int, error) {
+	var port int
+	var addrs []string
+	var err error
+
+	if lsn == nil || len(lsn.TCP) == 0 || len(lsn.UDP) == 0 ||
+		len(lsn.TCP) != len(lsn.UDP) {
+		return nil, 0, errBadSet
+	}
+
+	for i := range lsn.TCP {
+		tcp := lsn.TCP[i].Addr().(*net.TCPAddr)
+		udp := lsn.UDP[i].LocalAddr().(*net.UDPAddr)
+
+		if port == 0 {
+			port = tcp.Port
+		}
+
+		err = validatePair(tcp, udp, port)
+		if err != nil {
+			break
+		}
+
+		addrs = append(addrs, tcp.IP.String())
+	}
+
+	return addrs, port, err
+}
+
+func validatePair(tcp *net.TCPAddr, udp *net.UDPAddr, port int) error {
+	var err error
+
+	if port == 0 {
+		err = fmt.Errorf("invalid port: %s", tcp.String())
+	} else if !tcp.IP.Equal(udp.IP) || tcp.Port != udp.Port || tcp.Port != port {
+		err = core.Wrapf(errBadSet, "tcp:%s ≠ udp:%s (port:%v)",
+			tcp.String(), udp.String(), port)
+	}
+
+	return err
+}
+
+func newListeners(config *Config) (*Listeners, error) {
+	// parse BindAddr
+	addrs, err := config.Addresses()
+	if err != nil {
+		return nil, err
+	}
+
+	// listen ports
+	lsn := &Listeners{}
+	if n, err := lsn.listenConfig(addrs, config); err != nil {
+		return nil, err
+	} else if n < 1 {
+		return nil, errors.New("no listening ports")
+	} else {
+		return lsn, nil
+	}
+}
 
 // listenConfig attempts to set up listeners on all addresses based
 // on the nuances of a Config
-func (t *Transport) listenConfig(addrs []net.IP, config *Config) (int, error) {
+func (lsn *Listeners) listenConfig(addrs []net.IP, config *Config) (int, error) {
 	givenPort := config.BindPort
 
 	// when not strict, we try to multiple ports
@@ -14,7 +92,7 @@ func (t *Transport) listenConfig(addrs []net.IP, config *Config) (int, error) {
 		for i := 0; i < config.BindPortRetry; i++ {
 			var count int
 
-			count, err = t.tryListen(i, addrs, givenPort, config)
+			count, err = lsn.tryListen(i, addrs, givenPort, config)
 			if err == nil {
 				// success
 				return count, nil
@@ -26,14 +104,14 @@ func (t *Transport) listenConfig(addrs []net.IP, config *Config) (int, error) {
 	}
 
 	// but when strict, just once
-	return t.tryListen(0, addrs, givenPort, config)
+	return lsn.tryListen(0, addrs, givenPort, config)
 }
 
 // tryListen will try to setup listeners considering the attempt count.
 // when the port is 0, the first time it will try the default port, and
 // go random after that
 // when the port is non-zero, it will increment its value on each pass
-func (t *Transport) tryListen(pass int, addrs []net.IP, port int, config *Config) (int, error) {
+func (lsn *Listeners) tryListen(pass int, addrs []net.IP, port int, config *Config) (int, error) {
 	if port == 0 {
 		// the first time we try the default, on the next we
 		// go random
@@ -45,14 +123,14 @@ func (t *Transport) tryListen(pass int, addrs []net.IP, port int, config *Config
 		port = port + pass
 	}
 
-	return t.listen(addrs, port, config)
+	return lsn.listen(addrs, port, config)
 }
 
 // revive:disable:cognitive-complexity
 
 // listen attempts to listen all addresses on a given port,
 // and on success the listeners are stored on the Transport
-func (t *Transport) listen(addrs []net.IP, port int, config *Config) (int, error) {
+func (lsn *Listeners) listen(addrs []net.IP, port int, config *Config) (int, error) {
 	// revive:enable:cognitive-complexity
 	var ok bool
 
@@ -104,7 +182,7 @@ func (t *Transport) listen(addrs []net.IP, port int, config *Config) (int, error
 	}
 
 	ok = true
-	t.tcpListeners = tcpListeners
-	t.udpListeners = udpListeners
+	lsn.TCP = tcpListeners
+	lsn.UDP = udpListeners
 	return n, nil
 }
